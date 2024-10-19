87442a02f8a3ade30204a193a1ab18d7a4a78cdb
Orenoid <stefu1225@gmail.com>
2024-10-19 23:08:34 +0800

    fix: voice mode shortcut key

--- src/app/chat/components/input.tsx
+++ src/app/chat/components/input.tsx
@@ -324,6 +324,8 @@ export function MessageInput({
     </div>;
 }
 
+let enableVoiceModeShortcutTimer: NodeJS.Timeout
+
 function TextInput(
     { allowEdit, chatKey, addMessage, updateMessage, revisionMessage }: {
         allowEdit: boolean
@@ -500,8 +502,6 @@ function TextInput(
         setInputState({ type: 'voiceMode', autoSend: !inputState.autoSend })
     }
 
-    // let enableVoiceModeShortcutTimer: NodeJS.Timeout
-
     return <div ref={inputDivRef} tabIndex={0} className="flex flex-col focus:outline-none"
         onKeyDown={(e) => e.key === ' ' && isVoiceMode && startRecording()}
         // TODO bug: if the space key is released too soon right after pressing it, the recording will not stop
@@ -521,20 +521,19 @@ function TextInput(
             placeholder={isTyping ? `Type your message here...\n\nPress Enter to send, Ctrl+Enter to add the message, Shift+Enter to add a new line` : `Press Space to start recording, release to stop`}
             value={msg.content} onChange={(e) => setMsg(msg.updateContent(e.target.value))}
             readOnly={!isTyping}
-            // onKeyUp={(e) => {
-            //     if (e.key === 'v') {
-            //         clearTimeout(enableVoiceModeShortcutTimer)
-            //         setMsg(msg.updateContent(msg.content + 'v'))
-            //     }
-            // }}
+            onKeyUp={(e) => {
+                if (e.key === 'v') {
+                    clearTimeout(enableVoiceModeShortcutTimer)
+                    setMsg(msg.updateContent(msg.content + 'v'))
+                }
+            }}
             onKeyDown={(e) => {
-                // TODO bug exists, timeout can't be cleared
-                // if (e.key === 'v' && !(e.metaKey || e.ctrlKey) ) {
-                //     e.preventDefault()
-                //     enableVoiceModeShortcutTimer = setTimeout(() => {
-                //         enableVoiceMode()
-                //     }, 5000)
-                // }
+                if (e.key === 'v' && !(e.metaKey || e.ctrlKey) ) {
+                    e.preventDefault()
+                    enableVoiceModeShortcutTimer = setTimeout(() => {
+                        enableVoiceMode()
+                    }, 1000)
+                }
                 if (e.key === 'Enter' && e.ctrlKey) {
                     e.preventDefault();
                     handleSend(msg, { generateAssistantMsg: false });
