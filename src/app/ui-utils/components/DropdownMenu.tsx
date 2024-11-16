987e812e7846e84acd14489e092eb5ee0d4ae409
Orenoid <stefu1225@gmail.com>
2024-11-16 22:37:44 +0800

    refactor: implement DropDownMenu as one single component

--- src/app/ui-utils/components/DropdownMenu.tsx
+++ src/app/ui-utils/components/DropdownMenu.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaChevronDown } from 'react-icons/fa';
+import { TransparentOverlay } from './overlay';
 
-interface DropdownMenuProps {
+/** @deprecated Use DropDownMenuV2 instead */
+export function DropdownMenu({ menuItems, className }: {
     menuItems: { label: React.ReactNode; onClick: () => void }[];
     className?: string;
-}
-
-export function DropdownMenu({ menuItems, className }: DropdownMenuProps) {
+}) {
     return (
         <>
             <div className={`absolute z-50 bg-white border rounded-xl p-2 overflow-y-auto custom-scrollbar max-h-[300px] ${className}`}>
@@ -24,9 +24,47 @@ export function DropdownMenu({ menuItems, className }: DropdownMenuProps) {
     );
 }
 
+/** @deprecated Use DropDownMenuV2 instead */
 export function DropdownMenuEntry({ label, onClick, className }: { label: React.ReactNode; onClick: () => void; className?: string }) {
     return <div className={`flex flex-row items-center p-2 pl-3 cursor-pointer hover:bg-gray-100 rounded-md ${className}`} onClick={onClick}>
         <div className="mr-2">{label}</div>
         <FaChevronDown size={12} />
+    </div>
+}
+
+export function DropDownMenuV2({ entryLabel, entryClassName, menuItems, menuClassName }: {
+    entryLabel: React.ReactNode;
+    menuItems: { label: React.ReactNode; onClick: () => void }[];
+    entryClassName?: string;
+    menuClassName?: string;
+}) {
+    const [showMenu, setShowMenu] = useState(false)
+
+    return <div>
+        <div className="relative">
+            <div className="flex flex-row items-center hover:bg-gray-100 cursor-pointer rounded-md p-2 pl-3"
+                onClick={() => setShowMenu(!showMenu)}>
+                <div className="mr-2">{entryLabel}</div>
+                <FaChevronDown size={12} />
+            </div>
+            {/* dropdown menu */}
+            {showMenu &&
+                <div className={`absolute z-50 bg-white border rounded-xl p-2 overflow-y-auto custom-scrollbar max-h-[300px] top-full w-max ${menuClassName}`}>
+                    {menuItems.map((item, index) => (
+                        <div
+                            key={index}
+                            className="p-2 cursor-pointer hover:bg-gray-100 rounded-md whitespace-nowrap"
+                            onClick={() => {
+                                item.onClick();
+                                setShowMenu(false);
+                            }}
+                        >
+                            {item.label}
+                        </div>
+                    ))}
+                </div>
+            }
+        </div>
+        {showMenu && <TransparentOverlay onClick={() => setShowMenu(false)} />}
     </div>
 }
